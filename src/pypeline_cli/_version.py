ae103f774c66f55bd2d983bfe6f1c70c1303ac88
Daniel <daniel.brown1@cms.hhs.gov>
2025-12-18 15:49:16 -0500

    Created readme

--- src/pypeline_cli/_version.py
+++ src/pypeline_cli/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.0.3.dev7+gf44801839.d20251218"
-__version_tuple__ = version_tuple = (0, 0, 3, "dev7", "gf44801839.d20251218")
+__version__ = version = "0.3.1.dev0+gf1c9aa939.d20251218"
+__version_tuple__ = version_tuple = (0, 3, 1, "dev0", "gf1c9aa939.d20251218")
 
 __commit_id__ = commit_id = None
